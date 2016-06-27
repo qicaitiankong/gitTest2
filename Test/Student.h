1bf513b8a4c5b720be49037679f8dcee00b551e0
李自豪 <lizihao@lzhdemacbook-air.local>
2016-06-27 21:07:26 +0800

    我又添加了一个Student类

---
+++ Test/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  Test
+//
+//  Created by 李自豪 on 16/6/27.
+//  Copyright © 2016年 李自豪. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
---
+++ Test/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  Test
+//
+//  Created by 李自豪 on 16/6/27.
+//  Copyright © 2016年 李自豪. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
