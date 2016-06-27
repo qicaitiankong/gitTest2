671610fee6de132b5c6ab9a76076fc7f02a93be1
李自豪 <lizihao@lzhdemacbook-air.local>
2016-06-27 20:27:17 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by 李自豪 on 16/6/27.
+//  Copyright © 2016年 李自豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by 李自豪 on 16/6/27.
+//  Copyright © 2016年 李自豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
